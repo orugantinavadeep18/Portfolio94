cd23710beec135b44b497d8516d3817e261f912f
orugantinavadeep18 <orugantinavadeep@gmail.com>
2025-08-27 07:57:48 +0530

    Update data in contactus

--- my-portfolio/src/Components/contactus.jsx
+++ my-portfolio/src/Components/contactus.jsx
@@ -19,7 +19,7 @@ const ContactUs = () => {
     setStatus("");
 
     try {
-      const res = await fetch("http://localhost:5000/contact", {
+      const res = await fetch("https://portfolio94.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
